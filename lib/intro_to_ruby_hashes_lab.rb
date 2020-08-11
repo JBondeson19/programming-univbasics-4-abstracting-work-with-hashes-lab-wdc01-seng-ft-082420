1523c21659ad7c5ba459ba6f5fa9093058a7a0d8
James P Bondeson <jpbond58@gmail.com>
2020-08-11 00:39:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,8 +2,8 @@ def my_hash_creator(key, value)
   { key => value }
 end
 def read_from_hash(hash, key)
-  hash = { 'name' => "key"}
-  hash ['key']
+  hash = { 'name' => "Key"}
+  hash ['Key']
 
 
 end
